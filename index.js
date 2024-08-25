dd5b2cd2caa8eddabad5d56ad91b9334c9492835
Jax Choong <choongjax@gmail.com>
2024-08-25 23:49:36 +0800

    feat: now routes to different pages

--- index.js
+++ index.js
@@ -10,5 +10,23 @@ http.createServer(function(req,res){
             res.write(data);
             return res.end()
         })
+    }else if (path == "/about"){
+        fs.readFile('about.html',function(err,data){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end()
+        })
+    }else if (path == "/contact-me"){
+        fs.readFile('contact-me.html',function(err,data){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end()
+        })
+    }else{
+        fs.readFile('404.html',function(err,data){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end()
+        })
     }
 }).listen(8080)

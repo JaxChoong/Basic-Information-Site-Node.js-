5d8f624e733ce496dd428a53c6c7ee959014a4af
Jax Choong <choongjax@gmail.com>
2024-08-26 15:31:45 +0800

    feat: now runs with Express

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-var url = require("url");
 var fs = require('fs');
-var http = require("http")
+var express = require("express");
+var app = express()
 
-http.createServer(function(req,res){
-    var path = url.parse(req.url,true).pathname
-    if (path == "/"){
-        fs.readFile('index.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end()
-        })
-    }else if (path == "/about"){
-        fs.readFile('about.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end()
-        })
-    }else if (path == "/contact-me"){
-        fs.readFile('contact-me.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end()
-        })
-    }else{
-        fs.readFile('404.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            return res.end()
-        })
-    }
-}).listen(8080)
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end()
+    })
+})
+
+app.get('/about',(req,res)=>{
+    fs.readFile('about.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end()
+    })
+})
+
+app.get("/contact-me",(req,res)=>{
+    fs.readFile('contact-me.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end()
+    })
+})
+
+app.listen(PORT,()=> console.log(`Basic Information Site Running on port ${PORT}`))

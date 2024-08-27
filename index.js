a68fa2156b183561d58ca89e69feaced958c4932
Jax Choong <choongjax@gmail.com>
2024-08-27 22:53:09 +0800

    feat: now has routing to 404 page

--- index.js
+++ index.js
@@ -28,4 +28,11 @@ app.get("/contact-me",(req,res)=>{
     })
 })
 
+app.get("*",(req,res)=>{
+    fs.readFile('404.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end()
+    })
+})
 app.listen(PORT,()=> console.log(`Basic Information Site Running on port ${PORT}`))
